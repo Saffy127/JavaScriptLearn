36c7fa4ac284ca973e15dc1fb92a99919350f7a5
Saffy127 <88598834+Saffy127@users.noreply.github.com>
2023-06-12 18:08:10 -0600

    Create lesson1.js

---
+++ lesson1.js
@@ -0,0 +1,112 @@
+// You can declare a variable using any one of the three keywords: "var", "let" and "const"
+var boolean = true;
+let string = "Hello world!";
+const pi = 3.14159;
+
+// You can access a variable's value using its name
+console.log(boolean);
+console.log(string);
+console.log(pi);
+
+// You can assign a new value to a variable
+boolean = false;
+string = "The quick brown fox.";
+
+console.log(boolean); // -> false
+console.log(string);  // -> "The quick brown fox"
+
+// Write your own local variable here called name and make its value a string with your name in it
+// (Make sure you use the right keyword!)
+// If you get stuck, scroll down for a hint.
+let name = "Isaac";
+
+
+// Try making some more variables below and print them to the console
+
+let band = "Tool";
+let age = 22;
+let mind = "Harmonious"
+
+console.log(name);
+console.log(band);
+console.log(age);
+console.log(mind)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Here's a hint if you're stuck:
+// Local variables are declared with "let" or "const", you can use either. Here's an example with my name:
+/*
+let name = "Matthew";
+const name = "Matthew";
+*/
